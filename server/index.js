a6ee5a4d8e5b0ec13d0f40273ab82651765e8a4e
fryyyyy <frankyyyy@live.com>
2024-07-20 19:05:06 +0200

    error handling, remove unused packages

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 import express from "express";
-import mkcert from "vite-plugin-mkcert";
 import axios from "axios";
 import fs from "fs";
 import http from "http";
@@ -18,13 +17,19 @@ httpsServer.listen(PORT, () => {
   console.log("Our app is listening for request on port", PORT);
 });
 
+const config = {
+  timeout: 2000,
+};
+
 app.get("*", async (req, res) => {
   console.log("REQUEST", req.path);
   try {
-    const forward = await axios.get("http://localhost:31485" + req.path);
+    const forward = await axios.get("http://localhost:31485" + req.path, config);
     const data = forward?.data;
     res.send(data);
   } catch (error) {
-    console.error(error);
+    console.error(error.cause);
+    res.status(500);
+    res.send(error.cause);
   }
 });
